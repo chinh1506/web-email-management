a341901ed1175c431b457bbd7ec31e85916b7b3d
chinh1506 <chinhhoang115@gmail.com>
2022-05-31 11:04:38 +0700

    upload code

---
+++ src/main/java/com/example/webemailmanagement/WebEmailManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.webemailmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebEmailManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebEmailManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/webemailmanagement/WebEmailManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webemailmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebEmailManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
